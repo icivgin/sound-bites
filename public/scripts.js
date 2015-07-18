c80ae1e3a8e67e27129838af2fd64e66cd360432
Ian Civgin <imciv2@gmail.com>
2015-07-18 01:08:18 -0700

    apple api test

--- public/scripts.js
+++ public/scripts.js
@@ -14,38 +14,26 @@ $('#submit-track').on('submit', function(event) {
 	var word = { track: $('#track-name').val(), artist: $('#artist-name').val()};
 	var genre;
 
+	// $.get('https://developer.echonest.com/api/v4/artist/terms?api_key=' + apiKey + '&name=' + $('#artist-name').val() + '&format=json', function(data) {
+	// 		genre = data.response.terms[0].name; 
+	// 		genre2 = data.response.terms[1].name; 
+	// 		genre3 = data.response.terms[2].name; 
 
-	// $.get('https://api.spotify.com/v1/search?q=' + $('#artist-name').val() + '&type=artist', function(data) {
-	// 	console.log(data);
+	// 		// splitted = genre.split(' ');
+	// 		if(genre) {
+	// 			console.log(genre);
 
-	// 	genre = data.artists.items[0].genres[0]; 
+	// 			var genres = {genreOne: genre, genreTwo: genre2, genreThree: genre3};
 
-	// 	splitted = genre.split(' ');
+	// 			$('#result-display').html(template(genres));
+	// 			console.log(genre); 
+	// 		} else {
+	// 			console.log('sorry please try again');
+	// 		}
+	// 	});
 
-	// 	console.log(splitted);
-
-	// 	var genres = {genreOne: splitted[0], genreTwo: splitted[1], genreThree: splitted[0]};
-
-	// 	$('#result-display').html(template(genres));
-	// 	console.log(genre); 
-	// });
-
-	$.get('https://developer.echonest.com/api/v4/artist/terms?api_key=' + apiKey + '&name=' + $('#artist-name').val() + '&format=json', function(data) {
-			genre = data.response.terms[0].name; 
-			genre2 = data.response.terms[1].name; 
-			genre3 = data.response.terms[2].name; 
-
-			// splitted = genre.split(' ');
-			if(genre) {
-				console.log(genre);
-
-				var genres = {genreOne: genre, genreTwo: genre2, genreThree: genre3};
-
-				$('#result-display').html(template(genres));
-				console.log(genre); 
-			} else {
-				console.log('sorry please try again');
-			}
+	$.get('https://itunes.apple.com/search?term=taylor-swift&limit=1', function(data) {
+			console.log(data);
 		});
 
 });
