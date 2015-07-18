9b38f0927161ae1cce3873d3aaa3b3829810cf01
Ian Civgin <imciv2@gmail.com>
2015-07-18 01:09:47 -0700

    apple api test did not work

--- public/scripts.js
+++ public/scripts.js
@@ -14,26 +14,22 @@ $('#submit-track').on('submit', function(event) {
 	var word = { track: $('#track-name').val(), artist: $('#artist-name').val()};
 	var genre;
 
-	// $.get('https://developer.echonest.com/api/v4/artist/terms?api_key=' + apiKey + '&name=' + $('#artist-name').val() + '&format=json', function(data) {
-	// 		genre = data.response.terms[0].name; 
-	// 		genre2 = data.response.terms[1].name; 
-	// 		genre3 = data.response.terms[2].name; 
+	$.get('https://developer.echonest.com/api/v4/artist/terms?api_key=' + apiKey + '&name=' + $('#artist-name').val() + '&format=json', function(data) {
+			genre = data.response.terms[0].name; 
+			genre2 = data.response.terms[1].name; 
+			genre3 = data.response.terms[2].name; 
 
-	// 		// splitted = genre.split(' ');
-	// 		if(genre) {
-	// 			console.log(genre);
+			// splitted = genre.split(' ');
+			if(genre) {
+				console.log(genre);
 
-	// 			var genres = {genreOne: genre, genreTwo: genre2, genreThree: genre3};
+				var genres = {genreOne: genre, genreTwo: genre2, genreThree: genre3};
 
-	// 			$('#result-display').html(template(genres));
-	// 			console.log(genre); 
-	// 		} else {
-	// 			console.log('sorry please try again');
-	// 		}
-	// 	});
-
-	$.get('https://itunes.apple.com/search?term=taylor-swift&limit=1', function(data) {
-			console.log(data);
+				$('#result-display').html(template(genres));
+				console.log(genre); 
+			} else {
+				console.log('sorry please try again');
+			}
 		});
 
 });
