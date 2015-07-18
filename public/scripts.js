9f9e2140023fbdb055ac7bd398c1b3d512bf2e65
Ian Civgin <imciv2@gmail.com>
2015-07-18 00:56:03 -0700

    got some echo nest data

--- public/scripts.js
+++ public/scripts.js
@@ -3,13 +3,52 @@ $(function() {
 var source   = $("#test-template").html();
 var template = Handlebars.compile(source);
 
-$('#submit-track').on('click', function(event) {
+var apiKey = 'DGY3JGAZP1OFZR4RO';
+
+function convertArtist () {
+
+}
+
+$('#submit-track').on('submit', function(event) {
 	event.preventDefault();
 	var word = { track: $('#track-name').val(), artist: $('#artist-name').val()};
+	var genre;
 
-	console.log('hello butt face');
-	$('#result-display').html(template(word));
+
+	// $.get('https://api.spotify.com/v1/search?q=' + $('#artist-name').val() + '&type=artist', function(data) {
+	// 	console.log(data);
+
+	// 	genre = data.artists.items[0].genres[0]; 
+
+	// 	splitted = genre.split(' ');
+
+	// 	console.log(splitted);
+
+	// 	var genres = {genreOne: splitted[0], genreTwo: splitted[1], genreThree: splitted[0]};
+
+	// 	$('#result-display').html(template(genres));
+	// 	console.log(genre); 
+	// });
+
+	$.get('http://developer.echonest.com/api/v4/artist/terms?api_key=' + apiKey + '&name=' + $('#artist-name').val() + '&format=json', function(data) {
+			genre = data.response.terms[0].name; 
+			genre2 = data.response.terms[1].name; 
+			genre3 = data.response.terms[2].name; 
+
+			// splitted = genre.split(' ');
+			if(genre) {
+				console.log(genre);
+
+				var genres = {genreOne: genre, genreTwo: genre2, genreThree: genre3};
+
+				$('#result-display').html(template(genres));
+				console.log(genre); 
+			} else {
+				console.log('sorry please try again');
+			}
+		});
 
 });
+
 
 })
---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+// tell app to use bodyParser middleware
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(cors());
+
+// serve js and css files from public folder
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res, next) {
+	res.sendFile(__dirname + '/public/index.html')
+});
+
+
+app.listen(3000);
