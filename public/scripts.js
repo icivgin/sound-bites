eeedc1af22c64b340af96c6d8a47dfb656cdf074
Ian Civgin <imciv2@gmail.com>
2015-07-20 11:03:50 -0700

    added hide and show functionality for search and result views

--- public/scripts.js
+++ public/scripts.js
@@ -2,20 +2,10 @@ $(function() {
 
 //put this all in a controller
 
-// var map = L.map('map').setView([51.505, -0.09], 13);
+var searchSource = $('#search-template').html();
+var searchTemplate = Handlebars.compile(searchSource);
 
-// L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-//     attribution: 'test',
-//     maxZoom: 20,
-// }).addTo(map);
-
-var source   = $("#result-template").html();
-var template = Handlebars.compile(source);
-
-var foodSource   = $("#food-template").html();
-var foodTemplate = Handlebars.compile(foodSource);
-
-var finalSource   = $("#final-template").html();
+var finalSource = $('#result-template').html();
 var finalTemplate = Handlebars.compile(finalSource);
 
 var toggle = true;
@@ -25,8 +15,11 @@ var apiKey = 'DGY3JGAZP1OFZR4RO';
 var clientID = 'J3QA1RADSXWM43QI0VTSC1EFFCFFZYKPZW2Z2PNVL5YEWU5T';
 var clientSecret = 'TRR5QY22ZJWTTGWRXQ50MQIFXC0VHOLC2F3EPG2YRBMIFIXP';
 
-var trackName = $('#track-name').val();
-var artistName = $('#artist-name').val();
+var trackName = '';
+var artistName = '';
+
+//load search on page load
+$('#search-view').html(searchTemplate());
 
 String.prototype.capitalize = function(){
     return this.toLowerCase().replace( /\b\w/g, function (m) {
@@ -465,77 +458,16 @@ function getAlbumArt (trackName, artistName) {
 	});
 }
 
-function getVenue () {
-
-	// HANDLEBARS genre templateing
-	// var genres = {genreOne: genre1, genreTwo: genre2};
-	// $('#result-display-primary').html(template(genres));
-
-	// Map genre to food!
-	var mapResult = mapGenre(genre1, genre2);
-	
-	// HANDLEBARS food templating
-	// var food = { food: mapResult };
-	// $('#result-display-secondary').html(foodTemplate(food));
-
-	// make call to 4square api
-	$.get('https://api.foursquare.com/v2/venues/explore?client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20130815%20&near=san+francisco&limit=10&query=' + mapResult, function (data) {
-		console.log(data);
-		// checks that query returns 5 results
-		if(data.response.groups[0].items.length > 9) {
-			var ranVenue = Math.floor(Math.random() * 10);
-			var venue = data.response.groups[0].items[ranVenue].venue;
-			var trackName = $('#track-name').val().capitalize();
-			var artistName = $('#artist-name').val().capitalize();
-
-			var finalResult = {
-				trackNameResult: trackName,
-				artistNameResult: artistName,
-				albumArt: albumArtGlobal,
-				venueName: venue.name,
-				venueCat: venue.categories[0].name,
-				venueLat: venue.location.lat,
-				venueLng: venue.location.lng,
-				venueAddressA: venue.location.formattedAddress[0],
-				venueAddressB: venue.location.formattedAddress[1],
-				venueRating: venue.rating
-			};
-			
-			$('#result-display-tertiary').html(finalTemplate(finalResult));
-
-			//set up map
-			var map = L.map('map').setView([finalResult.venueLat, finalResult.venueLng], 15);
-
-			//add tile
-			//dark - http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png
-			//light - http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png
-			L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
-			    attribution: '',
-			    maxZoom: 20,
-			}).addTo(map);
-
-			//add marker
-			var marker = L.marker([finalResult.venueLat, finalResult.venueLng]).addTo(map);
-
-		} else {
-			alert('Oops! Something went wrong ... Try again!');
-		}
-	});
-}
-
-$('#submit-track').on('submit', function(event) {
-	event.preventDefault();
-	// var word = { track: $('#track-name').val(), artist: $('#artist-name').val()};
+function getResult (trackName, artistName) {
 
 	// check to see if song exists
-	$.get('https://developer.echonest.com/api/v4/song/search?api_key=DGY3JGAZP1OFZR4RO&format=json&results=6&artist=' + $('#artist-name').val() + '&title=' + $('#track-name').val(), function (data) {
+	$.get('https://developer.echonest.com/api/v4/song/search?api_key=DGY3JGAZP1OFZR4RO&format=json&results=6&artist=' + artistName + '&title=' + trackName, function (data) {
 		if (data.response.songs.length !== 0) {
 
-			getAlbumArt($('#track-name').val(), $('#artist-name').val());
+			getAlbumArt(trackName, artistName);
 
 			// query for primary and secondary genres
-				$.get('https://developer.echonest.com/api/v4/artist/terms?api_key=' + apiKey + '&name=' + $('#artist-name').val() + '&format=json', function(data) {
-					
+				$.get('https://developer.echonest.com/api/v4/artist/terms?api_key=' + apiKey + '&name=' + artistName + '&format=json', function(data) {
 					// query with secondary genre for more specificity
 					if (toggle) {	
 						genre1 = data.response.terms[0].name; 
@@ -543,7 +475,77 @@ $('#submit-track').on('submit', function(event) {
 
 						if (data.response.terms) {
 						
-						getVenue();
+						//get venue
+						// HANDLEBARS genre templateing
+						// var genres = {genreOne: genre1, genreTwo: genre2};
+						// $('#result-display-primary').html(template(genres));
+
+						// Map genre to food!
+						var mapResult = mapGenre(genre1, genre2);
+						
+						// HANDLEBARS food templating
+						// var food = { food: mapResult };
+						// $('#result-display-secondary').html(foodTemplate(food));
+
+						// make call to 4square api
+						$.get('https://api.foursquare.com/v2/venues/explore?client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20130815%20&near=san+francisco&limit=10&query=' + mapResult, function (data) {
+							// checks that query returns 5 results
+							if(data.response.groups[0].items.length > 9) {
+								var ranVenue = Math.floor(Math.random() * 10);
+								var venue = data.response.groups[0].items[ranVenue].venue;
+								var trackNameDeep = trackName.capitalize();
+								var artistNameDeep = artistName.capitalize();
+
+								var finalResult = {
+									trackNameResult: trackNameDeep,
+									artistNameResult: artistNameDeep,
+									albumArt: albumArtGlobal,
+									venueName: venue.name,
+									venueCat: venue.categories[0].name,
+									venueLat: venue.location.lat,
+									venueLng: venue.location.lng,
+									venueAddressA: venue.location.formattedAddress[0],
+									venueAddressB: venue.location.formattedAddress[1],
+									venueRating: venue.rating
+								};
+
+								$('#search-view').html('');
+								$('#result-view').html(finalTemplate(finalResult));
+
+
+								//on new search click
+								$('#new-search').on('click', function(event) {
+									event.preventDefault();
+									
+									$('#result-view').html('');
+									$('#search-view').html(searchTemplate());
+									addEventHandlers();
+								});
+
+								//on search again click
+								$('#search-again').on('click', function(event) {
+									event.preventDefault();
+									getResult(trackName, artistName);
+								})
+
+								//set up map
+								var map = L.map('map').setView([finalResult.venueLat, finalResult.venueLng], 15);
+
+								//add tile
+								//dark - http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png
+								//light - http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png
+								L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+								    attribution: '',
+								    maxZoom: 20,
+								}).addTo(map);
+
+								//add marker
+								var marker = L.marker([finalResult.venueLat, finalResult.venueLng]).addTo(map);
+
+							} else {
+								alert('Oops! Something went wrong ... Try again!');
+							}
+						});
 
 						//set toggle
 						toggle = true;
@@ -556,6 +558,23 @@ $('#submit-track').on('submit', function(event) {
 			alert('Seems like that song doesn\'t exist ... Try a different search!');
 		}
 	});
+}
+
+//on submit of search field
+$('#submit-track').on('submit', function(event) {
+	event.preventDefault();
+	// var word = { track: $('#track-name').val(), artist: $('#artist-name').val()};
+
+	getResult($('#track-name').val(), $('#artist-name').val());
 });
+
+function addEventHandlers () {
+	//on submit of search field
+	$('#submit-track').on('submit', function(event) {
+		event.preventDefault();
+		// var word = { track: $('#track-name').val(), artist: $('#artist-name').val()};
+		getResult($('#track-name').val(), $('#artist-name').val());
+	});
+};
 
 })
