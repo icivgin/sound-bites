bc703c1ca7a3045f01efa63972783a1a7b48c255
Ian Civgin <imciv2@gmail.com>
2015-07-21 11:09:45 -0700

    built out template for ME page

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -17,6 +17,34 @@ String.prototype.capitalize = function(){
     });
 };
 
+$('#results-view').on('click', '.result-div', function (event) {
+	console.log($(this));
+	var allResults = $('#results-view').children()[1].children;
+	for (i=0; i<allResults.length; i++) {
+		console.log(allResults[i]);
+		allResults[i].style.background = '#4FC1E9';
+		var marker = L.marker([finalResult.venueLat, finalResult.venueLng]).addTo(map);
+
+	}
+	$(this).css('background-color', 'green');
+});
+
+
+
+//set up map
+var map = L.map('map').setView([37.7833, -122.4167], 12);
+
+//add tile
+//dark - http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png
+//light - http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: '',
+    maxZoom: 20,
+    id: 'icivgin.b6d584ee',
+    accessToken: 'pk.eyJ1IjoiaWNpdmdpbiIsImEiOiI3MmVjZmMyNmM2ZWYxMGQ2MDAzMWU5MDhiZGI5ZmJkNSJ9.uOGm9rJve_i8WdJFKT3ljg'
+}).addTo(map);
+
+//add marker
 $.get('/v1/me', function (data) {
 	if(data) {
 		globalUserData = data;
